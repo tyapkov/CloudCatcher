84e2c94ae2a656aabb616681cf59aa38f753905f
Alexander Tyapkov <tyapkov@gmail.com>
2011-05-27 00:50:05 +0200

    CloudCatcher 2

---
+++ src/CloudCatcher/catcher/views.py
@@ -0,0 +1,21 @@
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    t = loader.get_template('catcher/index.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
+
+
+@csrf_exempt
+def save_image(request):
+    if request.POST:
+        # save it somewhere
+        f = open(settings.MEDIA_ROOT + '/webcamimages/someimage.jpg', 'wb')
+        f.write(request.raw_post_data)
+        f.close()
+        # return the URL
+        return HttpResponse('http://localhost:8080/site_media/webcamimages/someimage.jpg')
+    else:
+        return HttpResponse('no data')
